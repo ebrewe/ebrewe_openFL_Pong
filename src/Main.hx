6318ce1305080eb5d13b9391fdc1050b4132c956
ebrewe <ericdbrewer@gmail.com>
2016-03-05 14:15:28 -0500

    basic skeleton

--- src/Main.hx
+++ src/Main.hx
@@ -1,6 +1,8 @@
 package;
 
+import haxe.Timer;
 import openfl.display.Sprite;
+import openfl.events.Event;
 import openfl.Lib;
 
 /**
@@ -10,12 +12,46 @@ import openfl.Lib;
 class Main extends Sprite 
 {
 
+	var inited: Bool = false; 
+	
+	function resize(e)
+	{
+		if (!inited) init();
+	}
+	
+	function init()
+	{
+		if (inited) 
+			return;
+		inited = true; 
+	}
+	
 	public function new() 
 	{
 		super();
 		
-		// Assets:
-		// openfl.Assets.getBitmapData("img/assetname.jpg");
+		addEventListener(Event.ADDED_TO_STAGE, added); 
+	}
+	
+	function added(e)
+	{
+		removeEventListener(Event.ADDED_TO_STAGE, added);
+		stage.addEventListener(Event.RESIZE, resize);
+		
+		//conditionals
+		#if ios
+		Timer.delay(init, 100);
+		#else
+		init();
+		#end
+	}
+	
+	public static function main()
+	{
+		//static entry point
+		Lib.current.stage.align = flash.display.StageAlign.TOP_LEFT;
+		Lib.current.stage.scaleMode = flash.display.StageScaleMode.NO_SCALE;
+		Lib.current.addChild(new Main()); 
 	}
 
 }
